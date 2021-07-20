ca6114cd7190f075867ed5bb87f5cb6d2f7bd833
Miguel Garcia <miguelgarcia@pursuit.org>
2021-07-20 19:49:10 -0400

    added index and post query

--- back-end/queries/shoes.js
+++ back-end/queries/shoes.js
@@ -1,6 +1,30 @@
 const db = require("../db/config.js");
 
+const fetchAllShoes = async () => {
+  try {
+    const shoes = await db.any("SELECT * FROM shoes");
+    return { success: true, payload: shoes };
+  } catch (err) {
+    console.log(err);
+    return { success: false, payload: err };
+  }
+};
+
+const createShoe = async () => {
+  const { name } = newShoe;
+  try {
+    const shoe = await db.one(
+      "INSERT INTO shoes(name) VALUES($1) RETURNING *",
+      [name]
+    );
+    return { success: true, payload: shoe };
+  } catch (err) {
+    console.log(err);
+    return { success: false, payload: err };
+  }
+};
 
 module.exports = {
-    getAllShoes
-}
+  fetchAllShoes,
+  createShoe
+};
