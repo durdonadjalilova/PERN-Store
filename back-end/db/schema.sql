142a55bd8b09b25af250ea7220a5bf2950179948
Durdona Djalilova <ddjalilova3@gmail.com>
2021-07-20 20:41:34 -0400

    add table to heroku

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,6 +1,9 @@
-DROP TABLE IF EXISTS test;
-
-CREATE TABLE test (
-    id SERIAL PRIMARY KEY, 
-    name TEXT
+CREATE TABLE shoes (
+    id SERIAL PRIMARY KEY,
+    brand TEXT NOT NULL,
+    name TEXT NOT NULL,
+    image_url TEXT NOT NULL,
+    price NUMERIC NOT NULL,
+    size NUMERIC NOT NULL,
+    gender TEXT NOT NULL
 );
