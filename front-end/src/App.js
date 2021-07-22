98d3b5c9d917011fe07868553a5cda4e8175d794
Durdona Djalilova <ddjalilova3@gmail.com>
2021-07-21 20:48:22 -0400

    front-end routes complete

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,40 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./Components/NavBar";
+import Edit from "./Pages/Edit";
+import FourOFour from "./Pages/FourOFour";
 import Home from "./Pages/Home";
 import Index from "./Pages/Index";
 import New from "./Pages/New";
-import './App.css'
+import Show from "./Pages/Show";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/shoes">
-          <Index />
-        </Route>
-        <Route path="/shoes/new">
-          <New />
-        </Route>
-      </Switch>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/shoes">
+            <Index />
+          </Route>
+          <Route path="/shoes/new">
+            <New />
+          </Route>
+          <Route exact path="/shoes/:id">
+            <Show />
+          </Route>
+          <Route path="/shoes/:id/edit">
+            <Edit />
+          </Route>
+          <Route path="*">
+            <FourOFour />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
