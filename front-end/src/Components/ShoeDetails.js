49855f7f16f89f8e71931a6ec99a88ea9a778bdb
Durdona Djalilova <ddjalilova3@gmail.com>
2021-07-21 22:14:46 -0400

    details complete

--- front-end/src/Components/ShoeDetails.js
+++ front-end/src/Components/ShoeDetails.js
@@ -1,47 +1,49 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import axios from "axios";
-import apiURL from "../util/apiURL";
 import { Link } from "react-router-dom";
+import { apiURL } from "../util/apiURL";
 
 const API = apiURL();
 
+
 function ShoeDetails() {
-  let [selectedShoe, setSelectedShoe] = useState();
+  let [selectedShoe, setSelectedShoe] = useState({});
   let { id } = useParams();
   let history = useHistory();
 
-  const fetchShoe = async () => {
-    try {
-      let res = await axios.get(`${API}/shoes`);
-      console.log(res.data);
-      setSelectedShoe(res.data);
-    } catch (err) {
-      console.log(err);
-      return err;
-    }
-  };
+
 
   const deleteShoe = async (id) => {
     try {
-      let res = await axios.delete(`${API}/songs/${id}`);
-      setSelectedShoe(res.data);
+      let res = await axios.delete(`${API}/shoes/${id}`);
+      setSelectedShoe(res.data.payload);
     } catch (err) {
       console.log(err);
       return err;
     }
   };
 
-  const handleDelete = () => {
-    deleteShoe(id);
+  const handleDelete = async () => {
+    await deleteShoe(id);
     history.push("/shoes");
   };
 
   useEffect(() => {
+    const fetchShoe = async () => {
+      try {
+        let res = await axios.get(`${API}/shoes/${id}`);
+        console.log(res.data.payload);
+        setSelectedShoe(res.data.payload);
+      } catch (err) {
+        console.log(err);
+        return err;
+      }
+    };
     fetchShoe();
   }, [id]);
 
-  const { brand, name, image, price, size, gender } = shoe;
+  const { brand, name, image_url, price, size, gender } = selectedShoe;
 
   return <div>
       <Link to={"/shoes"}>
@@ -49,12 +51,12 @@ function ShoeDetails() {
       </Link>
       <button onClick={handleDelete}>DELETE</button>
       <br></br>
-      <p>{brand}</p>
-      <p>{name}</p>
-      <p>{image}</p>
-      <p>{price}</p>
-      <p>{size}</p>
-      <p>{gender}</p>
+      <p>Brand: {brand} </p>
+      <p>Name: {name}</p>
+      <img src={image_url} alt="shoe" />
+      <p>Price: {price}</p>
+      <p>Size: {size}</p>
+      <p>Gender: {gender}</p>
   </div>;
 }
 
